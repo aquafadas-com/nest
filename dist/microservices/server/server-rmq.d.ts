fc33b566e81a9bfe67ac8a1920c5acf8a1e823f4
steve.campos <steve.campos@rakuten.com>
2020-10-09 20:43:14 +0200

    feat(dist): deploy dist

---
+++ dist/microservices/server/server-rmq.d.ts
@@ -0,0 +1,22 @@
+import { Transport } from '../enums';
+import { CustomTransportStrategy, RmqOptions } from '../interfaces';
+import { Server } from './server';
+export declare class ServerRMQ extends Server implements CustomTransportStrategy {
+    protected readonly options: RmqOptions['options'];
+    readonly transportId = Transport.RMQ;
+    protected server: any;
+    protected channel: any;
+    protected readonly urls: string[];
+    protected readonly queue: string;
+    protected readonly prefetchCount: number;
+    protected readonly queueOptions: any;
+    protected readonly isGlobalPrefetchCount: boolean;
+    constructor(options: RmqOptions['options']);
+    listen(callback: () => void): Promise<void>;
+    close(): void;
+    start(callback?: () => void): Promise<void>;
+    createClient<T = any>(): T;
+    setupChannel(channel: any, callback: Function): Promise<void>;
+    handleMessage(message: Record<string, any>, channel: any): Promise<void>;
+    sendMessage<T = any>(message: T, replyTo: any, correlationId: string): void;
+}
